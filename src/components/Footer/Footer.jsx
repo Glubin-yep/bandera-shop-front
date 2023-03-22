a546eed39f8d5bc916fe4accb88365460b4e0865
Dmytro Chyr <88516266+Glubin-yep@users.noreply.github.com>
2023-03-22 18:02:40 +0200

    Fix URL

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,9 +8,9 @@ function Footer() {
         <footer className="footer">
             <div className="icons">
                 <a href="https://www.youtube.com/"><img src={youtube} className="icon" alt=""/></a>
-                <a href="https://www.instagram.com/"><img src={telegram} className="icon" alt=""/></a>
+                <a href="https://web.telegram.org/k/"><img src={telegram} className="icon" alt=""/></a>
                 <a href="https://www.facebook.com/"><img src={facebook} className="icon" alt=""/></a>
-                <a href="https://web.telegram.org/k/"><img src={instagram} className="icon" alt=""/></a>
+                <a href="https://www.instagram.com/"><img src={instagram} className="icon" alt=""/></a>
             </div>
             <div className="copyright">
                 <span className="copyright--text">© 2023 Bandera Shop</span>
