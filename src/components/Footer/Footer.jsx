62c2b84f03573ae8af5241b5ea4fcf6c1cfee14f
Denys Markevych <denysjse@gmail.com>
2023-03-22 13:46:13 +0200

    fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,10 +7,10 @@ function Footer() {
     return(
         <div className="footer">
             <div className="icons">
-                <img src={youtube} className="icon"/>
-                <img src={telegram} className="icon"/>
-                <img src={facebook} className="icon"/>
-                <img src={instagram} className="icon"/>
+                <img src={youtube} className="icon" alt=""/>
+                <img src={telegram} className="icon" alt=""/>
+                <img src={facebook} className="icon" alt=""/>
+                <img src={instagram} className="icon" alt=""/>
             </div>
             <div className="copyright">
                 <span className="copyright--text">© 2023 Bandera Shop</span>
