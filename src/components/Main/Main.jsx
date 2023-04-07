cbc5bf64c3ebbd571f828d9b44123f5dedf3b2ed
Dmytro Chyr <dima543439@gmail.com>
2023-04-08 02:40:59 +0300

    Remove cheack auth from main page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,44 +1,40 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../../style.css";
-import { Context } from "../../index";
 import LoadingScreen from "../loading/loading";
 import ItemsService from "../../services/ItemsService";
 import CategorySection from "./CategorySection";
 
 function Main() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const { store } = useContext(Context);
+  //const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //const { store } = useContext(Context);
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    const checkAuthentication = async () => {
-      await store.checkAuth();
-      console.log(store.isAuthenticated);
-      if (store.isAuthenticated) {
-        setIsLoggedIn(true);
-      } else {
-        console.log("Авторизуйтеся");
-      }
-    };
+    //const checkAuthentication = async () => {
+    //await store.checkAuth();
+    // console.log(store.isAuthenticated);
+    //  if (store.isAuthenticated) {
+    //   setIsLoggedIn(true);
+    //} else {
+    //    console.log("Авторизуйтеся");
+    //  }
+    // };
     setIsLoading(true);
-    checkAuthentication();
-    getData();    
+    //checkAuthentication();
+    getData();
     setIsLoading(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [store]);
+  }, []);
 
-  
-  
   async function getData() {
     const newData = ItemsService.getAllProducts();
     setData((await newData).data);
     console.log(data);
   }
 
-
   return (
-    <div className={`main ${isLoggedIn ? "" : "main blurred--div"}`}>
+    <div className="main">
       {isLoading && <LoadingScreen />}
 
       <CategorySection
