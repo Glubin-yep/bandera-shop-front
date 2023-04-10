76c30ee5b352068b1b77b72c3b5eceb4590c43c0
Dmytro Chyr <dima543439@gmail.com>
2023-04-10 22:52:05 +0300

    Fix api link

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = `http://localhost:4000/api`
+export const API_URL = `https://bandera-shop-back.onrender.com/api`
 
 const api = axios.create({
     withCredentials: true,
